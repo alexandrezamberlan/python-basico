650c9b374b19d871eddec728b5a6765544a174e0
Alexandre de O. Zamberlan <alexandre.o.zamberlan@gmail.com>
2020-08-25 09:53:51 -0300

    revisao python

---
+++ revisao_python/prog1.py
@@ -0,0 +1,27 @@
+# python organiza o fluxo das instruções pela edentação/indentação
+
+print('Olá pessoal!!')
+
+
+#variáveis
+nome = 'Alexandre Zamberlan'
+print(nome)
+
+x = y = z = 0
+print(x)
+print(y)
+print(z)
+
+x, y, z = 10, 25, 30
+print(x)
+print(y)
+print(z)
+
+
+"""
+as instruções de controle: seleção e repetição 
+têm o mesmo comportamento das outras linguagens,
+porém, possuem sintaxe específica
+"""
+
+
