fd08e0a62422c5cec88204cbc21a945d1d5daa32
Alexandre de O. Zamberlan <alexandre.o.zamberlan@gmail.com>
2020-08-25 10:29:17 -0300

    prog2.py

--- revisao_python/prog1.py
+++ revisao_python/prog1.py
@@ -1,11 +1,24 @@
 # python organiza o fluxo das instruções pela edentação/indentação
-
 print('Olá pessoal!!')
-
+print('Olá "Alex" meu amigo')
 
 #variáveis
-nome = 'Alexandre Zamberlan'
+nome = 'Pedro Ernesto'
+idade = 45
 print(nome)
+print(idade)
+
+"""
+as instruções de controle: seleção e repetição 
+têm o mesmo comportamento das outras linguagens,
+porém, possuem sintaxe específica
+"""
+
+if (idade > 18):
+    print('Você já tem maioridade')
+    print('Inclusive você já dormiu ', idade/3, ' anos')
+elif (idade > 10):
+    print('Você ainda possui responsável')
 
 x = y = z = 0
 print(x)
@@ -18,10 +31,5 @@ print(y)
 print(z)
 
 
-"""
-as instruções de controle: seleção e repetição 
-têm o mesmo comportamento das outras linguagens,
-porém, possuem sintaxe específica
-"""
 
 
---
+++ revisao_python/prog2.py
@@ -0,0 +1,16 @@
+lista_alunos = ['marconatto', 'andrew','lucas','andrew']
+nome_aluno = lista_alunos[1]
+
+print('lista_alunos tem tipo: ', type(lista_alunos))
+print(type(nome_aluno))
+
+#coersão em tempo de programação, ou seja, mudar o tipo
+x = 'Lucas'
+print(x)
+x = 3.4
+print(x)
+y = 3 + x
+print(y)
+
+# print('Qual sua idade: ')
+idade = input('Qual sua idade: ')
