932353b0576364c825f468a2381c83156b3e213f
Alexandre Zamberlan <alexandre.o.zamberlan@gmail.com>
2022-11-10 21:54:06 -0300

    exercício analise palavra

--- 9_analisePalavrasArquivo/analisePalavras1.py
+++ 9_analisePalavrasArquivo/analisePalavras1.py
@@ -1,7 +1,6 @@
 from __future__ import with_statement
 import os
 
-
 class Palavra:
   valor = ""
   quantidade = 0
@@ -9,38 +8,68 @@ class Palavra:
   def __init__(self, valor):
     self.valor = valor
     self.quantidade = 1
+    
+  def __eq__(self, other):
+    if isinstance(other, Palavra):
+        return self.valor == other.valor
+    return False
+    
+  def __gt__(self, other):
+    if self.quantidade == other.quantidade:
+        return self.valor < other.valor
+    return self.quantidade > other.quantidade
 
 ################################################
-def abrir_arquivo_texto():
-  nome_arquivo = input("Digite caminho e nome do arquivo texto: ")
-  print("\n")
-  try:
-    with open(nome_arquivo, "r", encoding='utf8') as leitor:
-        for linha in leitor:
-            print(linha)
-  except:
-    print("Arquivo com problema ou não localizado")
+class Util:
+  @staticmethod
+  def abrir_arquivo_texto():
+    nome_arquivo = input("Digite caminho e nome do arquivo texto: ")
+    print("\n")
+    try:
+      with open(nome_arquivo, "r", encoding='utf8') as leitor:
+          for linha in leitor:
+              print(linha)
+    except:
+      print("Arquivo com problema ou não localizado")
+    
+    return nome_arquivo
   
-  return nome_arquivo
-  
+  @staticmethod
+  def criar_lista_palavras(lista_palavras, nome_arquivo):
+    try:
+      with open(nome_arquivo, "r", encoding='utf8') as leitor:
+          for linha in leitor:
+            
+            linha = linha.replace(".","")
+            linha = linha.replace(";","")
+            linha = linha.replace(",","")
+            linha = linha.replace("!","")
+            linha = linha.replace("?","")
+            linha = linha.replace("\n","")
+            
+            dados_linha = linha.split(" ")
+           
+            for palavra in dados_linha:
+              palavra = palavra.lower()
+              
+              palavra_oo = Palavra(palavra)
 
-def criar_lista_palavras(lista_palavras, nome_arquivo):
-  try:
-    with open(nome_arquivo, "r", encoding='utf8') as leitor:
-        for linha in leitor:
-          dados_linha = linha.split(" ")
-          for palavra in dados_linha:
-            palavra = palavra.lower()
+              encontrado = False
+              for p in lista_palavras:
+                if (p.valor == palavra_oo.valor):
+                  p.quantidade += 1
+                  encontrado = True   
+                
+              #saindo do 2n for, significa que palavra_oo nao esta na lista, vamos adiciona-la 
+              if (not encontrado):
+                lista_palavras.append(palavra_oo)          
+    except:
+      print("Arquivo com problema ou não localizado")
 
-            if (not palavra in lista_palavras):
-              lista_palavras.append(palavra)          
-  except:
-    print("Arquivo com problema ou não localizado")
-
-
-def exibir_lista_palavras(lista_palavras):
-  for palavra in lista_palavras:
-    print(palavra)
+  @staticmethod
+  def exibir_lista_palavras(lista_palavras):
+    for palavra in lista_palavras:
+      print(palavra.valor, " - ", palavra.quantidade)
 
 #######################################
 ## parte principal do sistema
@@ -49,7 +78,7 @@ lista_palavras = []
 nome_arquivo = ""
 
 while (True):
-  os.system("clear")
+  # os.system("clear")
   print("MENU")
   print("1 - Carregar arquivo texto")
   print("2 - Listar palavras e suas ocorrências")
@@ -58,19 +87,20 @@ while (True):
 
   if (opcao == 1):
     print("Abrindo o arquivo....")
-    nome_arquivo = abrir_arquivo_texto()
+    nome_arquivo = Util.abrir_arquivo_texto()
   elif (opcao == 2):
     print("Lista de palavras....")
     if (nome_arquivo == ""):
       print("Primeiro use a opção 1, para carregar o arquivo texto")
     else:
       lista_palavras.clear()
-      criar_lista_palavras(lista_palavras, nome_arquivo)
-      exibir_lista_palavras(lista_palavras)
+      Util.criar_lista_palavras(lista_palavras, nome_arquivo)
+      lista_palavras.sort(reverse=True)
+      Util.exibir_lista_palavras(lista_palavras)
   elif (opcao == 3):
     print("Obrigado por usar o sistema")
     break
   else: 
     print("Opção inválida!")
 
-  os.system("sleep 4")
+  # os.system("sleep 4")
